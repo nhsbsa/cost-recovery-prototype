195f8ab0b0b4eb39f48f0d19d748d78a568dacb1
lakshmi Karuppiah <lakshmi.karuppiah@nhs.net>
2020-07-13 12:50:00 +0100

    Third commit

--- app/views/current/_routes.js
+++ app/views/current/_routes.js
@@ -1,4 +1,26 @@
 const express = require('express')
 const router = express.Router()
+
+router.get('/', function(req , res){ 
+  res.render('index');
+ });
+
 // Add your routes here - above the module.exports line
-module.exports = router
+
+  router.post('/test', function (req, res) {
+
+    var example = req.session.data['example']
+  
+    if (example == "yes"){
+      res.redirect('layout')
+    }
+    if (example == "no"){
+      res.redirect('index')
+    }
+    else {
+      res.redirect('page')
+    }
+  
+  })
+
+  module.exports = router
