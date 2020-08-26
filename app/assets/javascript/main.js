96407887aae4103740417d61d55e4d18483423d5
lakshmi Karuppiah <lakshmi.karuppiah@nhs.net>
2020-08-26 18:41:30 +0100

    removed js in public folder

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -66,3 +66,10 @@ function display1(){
 function display2(){
     document.getElementById('div1').style.display = 'none';
   }
+
+  function display1(){
+    document.getElementById('div1').style.display ='block';
+  }
+function display2(){
+    document.getElementById('div1').style.display = 'none';
+  }
