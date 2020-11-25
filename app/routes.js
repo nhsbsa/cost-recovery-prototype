65d97de5e9972d1c89f4f7174ebfb97260677aaf
lakshmi Karuppiah <lakshmi.karuppiah@nhs.net>
2020-11-25 12:55:33 +0000

    reports content change

--- app/routes.js
+++ app/routes.js
@@ -37,6 +37,10 @@ router.use('/sprint-8/search', require('./views/sprint-8/_routes'));
 
 router.use('/sprint-8/search-no-results', require('./views/sprint-8/_routes'));
 
+router.use('/sprint-9/search', require('./views/sprint-9/_routes'));
+
+router.use('/sprint-9/search-no-results', require('./views/sprint-9/_routes'));
+
 
 
 module.exports = router;
